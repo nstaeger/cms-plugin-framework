de6114c043004d23c928fd967ca0010a88df5f6e
nstaeger <mail@nstaeger.de>
2016-05-27 19:26:01 +0200

    feat(DatabaseBroker): throw DatabaseException when error occurred

--- src/Broker/Wordpress/WordpressDatabaseBroker.php
+++ src/Broker/Wordpress/WordpressDatabaseBroker.php
@@ -3,6 +3,7 @@
 namespace Nstaeger\CmsPluginFramework\Broker\Wordpress;
 
 use Nstaeger\CmsPluginFramework\Broker\DatabaseBroker;
+use Nstaeger\CmsPluginFramework\Database\DatabaseException;
 use wpdb;
 
 class WordpressDatabaseBroker implements DatabaseBroker
@@ -19,22 +20,29 @@ class WordpressDatabaseBroker implements DatabaseBroker
 
     public function delete($table, array $where)
     {
-        return $this->databaseConnection->delete($this->parsePrefix($table), $where);
+        $result = $this->databaseConnection->delete($this->parsePrefix($table), $where);
+        $this->wasGood($result);
+
+        return $result;
     }
 
     public function executePreparedQuery($query, $args)
     {
         $prepared = $this->databaseConnection->prepare($query, $args);
         $parsed = $this->parsePrefix($prepared);
+        $result = $this->databaseConnection->query($parsed);
+        $this->wasGood($result);
 
-        return $this->databaseConnection->query($parsed);
+        return $result;
     }
 
     public function executeQuery($query)
     {
         $parsed = $this->parsePrefix($query);
+        $result =  $this->databaseConnection->query($parsed);
+        $this->wasGood($result);
 
-        return $this->databaseConnection->query($parsed);
+        return $result;
     }
 
     public function fetchAll($query)
@@ -42,18 +50,46 @@ class WordpressDatabaseBroker implements DatabaseBroker
         return $this->databaseConnection->get_results($this->parsePrefix($query), ARRAY_A);
     }
 
+    function getLastError()
+    {
+        return $this->databaseConnection->last_error;
+    }
+
+    function getLastInsertedId()
+    {
+        return $this->databaseConnection->insert_id;
+    }
+
+    function getLastQuery()
+    {
+        return $this->databaseConnection->last_query;
+    }
+
     public function insert($table, array $data, $format = null)
     {
-        return $this->databaseConnection->insert($this->parsePrefix($table), $data, $format);
+        $result = $this->databaseConnection->insert($this->parsePrefix($table), $data, $format);
+        $this->wasGood($result);
+
+        return $result;
     }
 
     public function update($table, array $data, array $where, $format = null, $where_format = null)
     {
-        return $this->databaseConnection->update($this->parsePrefix($table), $data, $where, $format, $where_format);
+        $result = $this->databaseConnection->update($this->parsePrefix($table), $data, $where, $format, $where_format);
+        $this->wasGood($result);
+
+        return $result;
     }
 
     private function parsePrefix($query)
     {
         return str_replace("@@", $this->databaseConnection->prefix, $query);
+    }
+
+    private function wasGood($result)
+    {
+        if (!$result) {
+            throw new DatabaseException($this->getLastError(), $this->getLastQuery());
+        }
     }
 }
